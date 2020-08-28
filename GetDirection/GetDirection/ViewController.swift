0d29cfee50a99c434b756e014d9de3556dffbb65
Abdul Sami Sultan <56970013+AbdulSami-Sultan@users.noreply.github.com>
2020-08-28 22:23:11 +0500

    Add files via upload
    Initial commit

---
+++ GetDirection/GetDirection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetDirection
+//
+//  Created by Abdul Sami Sultan on 28/08/2020.
+//  Copyright © 2020 Sami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
