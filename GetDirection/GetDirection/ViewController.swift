1340c54e891c73f83084a84fe9bc2ae0e667be33
Abdul Sami Sultan <abdulsami923@gmail.com>
2020-08-28 22:49:06 +0500

    coordinates

--- GetDirection/GetDirection/ViewController.swift
+++ GetDirection/GetDirection/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+ 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var getDirectionButton: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.title = "Map"
+        navigationController?.navigationBar.prefersLargeTitles = true
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getDirectionTapped(_ sender: Any) {
+        getAddress()
+    }
+    func getAddress(){
+        print(textField.text!)
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(textField.text!) { (placemarks, error) in
+            guard let placemarks = placemarks, let location = placemarks.first?.location else{
+                return
+            }
+            print(location)
+        }
+    }
 }
 
